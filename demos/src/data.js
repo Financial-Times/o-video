b190dadee45bc4dd199259eba8408f6a8de4764e
Paul Wilson <paul.i.wilson@ft.com>
2016-08-18 16:52:17 +0100

    opacity behind play icon

--- demos/src/data.js
+++ demos/src/data.js
@@ -1,8 +1,11 @@
+// this is so we can see the component with different videos as lightness of placeholder image and length of title / description can vary wildly
+
 
 const ids = [
 	"5086559154001",
 	"5086528258001",
-	"5084369376001"
+	"5084369376001",
+	"5087557269001"
 ];
 
 function randomId(){
