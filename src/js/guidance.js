b0ebbcc3fa50de3935d980f226313d3619bafcc7
Rob Squires <rob.squires@ft.com>
2019-07-03 13:59:44 +0100

    style guidance banner

--- src/js/guidance.js
+++ src/js/guidance.js
@@ -1,7 +1,7 @@
 
 const closeButton = (onClick) => {
 	const button = document.createElement('button');
-	button.className = 'o-buttons o-buttons--inverse o-buttons-icon o-buttons-icon--cross';
+	button.className = 'o-video__guidance__close';
 	button.addEventListener('click', e => {
 		e.stopPropagation();
 		onClick()
@@ -11,21 +11,16 @@ const closeButton = (onClick) => {
 
 const container = () => {
 	const containerEl = document.createElement('div');
-	containerEl.className = 'o-video__guidance';
+	containerEl.className = `o-video__guidance ${bannerMode && 'o-video__guidance--banner'}`;
 	return containerEl;
 }
 
 const link = () => {
 	const linkEl = document.createElement('a');
 	linkEl.setAttribute('href', 'https://www.ft.com/accessibility');
-	linkEl.className = 'o-video__guidance__text';
+	linkEl.className = 'o-video__guidance__link';
 	linkEl.innerText = 'Subtitles unavailable';
 	linkEl.target = '_blank';
-	
-	const title = 'Subtitles unavailable, find out why here';
-	linkEl.setAttribute('aria-label',  title);
-	linkEl.title =  title;
-
 	linkEl.addEventListener('click', e => e.stopPropagation())
 	return linkEl;
 }
